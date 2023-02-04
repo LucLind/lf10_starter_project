eef4021d3ff584b53e15449596fd1b5314181fbe
LucLind <luclindemann@gmail.com>
2023-02-04 13:23:29 +0100

    Styling Issues and Animations
    Fixed some styling issues and added some fancy pants animations ✨

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -3,6 +3,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { Employee } from '../Employee';
+import { Qualification } from '../Qualification';
 
 @Component({
   selector: 'app-employee-details',
@@ -16,9 +17,27 @@ export class EmployeeDetailsComponent {
   @Input()
   employee!: Employee;
   id;
+  public qualificationOptions : Observable<Qualification[]> = of();
+  public employeeQualifications : Qualification[] = [];
 
   constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
     this.id = this.activatedRoute.snapshot.paramMap.get('id');
     
   }
+
+  ngOnInit(): void {
+    this.qualificationOptions = this.http.get<Qualification[]>('/qualificationsService', {
+      headers: new HttpHeaders()
+        .set('Content-Type', 'application/json')
+    });
+    
+    this.http.get<any>(`/employeeService/${this.id}/qualifications`, {
+      headers: new HttpHeaders()
+        .set('Content-Type', 'application/json')
+    }).subscribe(data => {
+      this.employeeQualifications = data.skillSet;
+    });
+  }
+
+
 }
